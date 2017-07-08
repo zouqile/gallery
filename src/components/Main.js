939983d31f45f244ae717c656268aad6e18768ec
zouqile <zouqile@yoju360.com>
2017-07-08 15:50:20 +0800

    1，控制组件

--- src/components/Main.js
+++ src/components/Main.js
@@ -84,6 +84,41 @@ var ImgFigure = React.createClass(
   }
 );
 
+// 控制组件
+var ControllerUnit = React.createClass({
+    handleClick: function (e) {
+
+        // 如果点击的是当前正在选中态的按钮，则翻转图片，否则将对应的图片居中
+        if (this.props.arrange.isCenter) {
+            this.props.inverse();
+        } else {
+            this.props.center();
+        }
+
+        e.preventDefault();
+        e.stopPropagation();
+    },
+    render: function () {
+        var controlelrUnitClassName = "controller-unit";
+
+        // 如果对应的是居中的图片，显示控制按钮的居中态
+        if (this.props.arrange.isCenter) {
+            controlelrUnitClassName += " is-center";
+
+            // 如果同时对应的是翻转图片， 显示控制按钮的翻转态
+            if (this.props.arrange.isInverse) {
+                controlelrUnitClassName += " is-inverse";
+            }
+        }
+
+        return (
+            <span className={controlelrUnitClassName} onClick={this.handleClick}></span>
+        );
+    }
+});
+
+
+
 
 var AppComponent = React.createClass({
   Constant:
@@ -289,6 +324,9 @@ var AppComponent = React.createClass({
       imgFigures.push(<ImgFigure key={index} data={value} ref={'imgFigure' + index}
         arrange={this.state.imgsArrangeArr[index]} inverse={this.inverse(index)}
         center={this.center(index)} />);
+
+         controllerUnits.push(<ControllerUnit key={index} arrange={this.state.imgsArrangeArr[index]} inverse={this.inverse(index)} center={this.center(index)}/>);
+
     }.bind(this));
 
     return (
